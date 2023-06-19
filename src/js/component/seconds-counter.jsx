9d5a9a03d6dcb4bef14cdf0ad775135c002db6bd
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-06-19 11:22:38 +0000

    add a bit of style

--- src/js/component/seconds-counter.jsx
+++ src/js/component/seconds-counter.jsx
@@ -3,13 +3,36 @@ import "../../styles/index.css"
 
 const SecondsCounter = (props) => {
     return (
-    <div className="text-light" >
+    <div className="row d-flex justify-content-center" >
+        
+        <div className="card clock-style" style={{width: "4rem"}}>
+        <i className="fa-regular fa-clock"></i>
+        </div>
+
+        <div className="card" style={{width: "4rem"}}>
         {props.digito6}
+        </div>
+        <div className="card" style={{width: "4rem"}}>
         {props.digito5}
+        </div>
+        <div className="card" style={{width: "4rem"}}>
+        {props.digito4}
+        </div>
+        <div className="card" style={{width: "4rem"}}>
+        {props.digito3}
+        </div>
+        <div className="card" style={{width: "4rem"}}>
+        {props.digito2}
+        </div>
+        <div className="card" style={{width: "4rem"}}>
+        {props.digito1}
+        </div>
+        
+        {/* {props.digito5}
         {props.digito4}
         {props.digito3}
         {props.digito2}
-        {props.digito1}
+        {props.digito1} */}
         
     </div>
     )
